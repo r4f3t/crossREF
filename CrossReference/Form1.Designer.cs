50316312ac900c98919753cfee54039f53cf2d8a
Rafet PArlak <parlakrafet01@gmail.com>
2019-02-21 00:06:28 +0300

    first r4f3t

--- CrossReference/Form1.Designer.cs
+++ CrossReference/Form1.Designer.cs
@@ -28,13 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.TXTCode = new System.Windows.Forms.TextBox();
+            this.BTNAra = new System.Windows.Forms.Button();
+            this.BTNAktar = new System.Windows.Forms.Button();
+            this.GridItems = new System.Windows.Forms.DataGridView();
+            this.ITEM_CODE = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GridGezgin = new System.Windows.Forms.DataGridView();
+            this.gezginItem = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Class = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.GridItems)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GridGezgin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TXTCode
+            // 
+            this.TXTCode.Font = new System.Drawing.Font("Tahoma", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TXTCode.Location = new System.Drawing.Point(0, 0);
+            this.TXTCode.Name = "TXTCode";
+            this.TXTCode.Size = new System.Drawing.Size(491, 36);
+            this.TXTCode.TabIndex = 0;
+            this.TXTCode.TextChanged += new System.EventHandler(this.TXTCode_TextChanged);
+            this.TXTCode.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TXTCode_KeyDown);
+            this.TXTCode.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TXTCode_KeyPress);
+            this.TXTCode.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TXTCode_KeyUp);
+            // 
+            // BTNAra
+            // 
+            this.BTNAra.Location = new System.Drawing.Point(491, 0);
+            this.BTNAra.Name = "BTNAra";
+            this.BTNAra.Size = new System.Drawing.Size(142, 34);
+            this.BTNAra.TabIndex = 1;
+            this.BTNAra.Text = "Ara";
+            this.BTNAra.UseVisualStyleBackColor = true;
+            this.BTNAra.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BTNAktar
+            // 
+            this.BTNAktar.Location = new System.Drawing.Point(633, 0);
+            this.BTNAktar.Name = "BTNAktar";
+            this.BTNAktar.Size = new System.Drawing.Size(164, 34);
+            this.BTNAktar.TabIndex = 1;
+            this.BTNAktar.Text = "Excelden Aktar";
+            this.BTNAktar.UseVisualStyleBackColor = true;
+            this.BTNAktar.Click += new System.EventHandler(this.BTNAktar_Click);
+            // 
+            // GridItems
+            // 
+            this.GridItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridItems.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ITEM_CODE});
+            this.GridItems.Location = new System.Drawing.Point(0, 34);
+            this.GridItems.Name = "GridItems";
+            this.GridItems.RowTemplate.Height = 24;
+            this.GridItems.Size = new System.Drawing.Size(797, 413);
+            this.GridItems.TabIndex = 2;
+            // 
+            // ITEM_CODE
+            // 
+            this.ITEM_CODE.DataPropertyName = "ITEMCODE";
+            this.ITEM_CODE.HeaderText = "Ürün Kodu";
+            this.ITEM_CODE.Name = "ITEM_CODE";
+            this.ITEM_CODE.ReadOnly = true;
+            this.ITEM_CODE.Width = 750;
+            // 
+            // GridGezgin
+            // 
+            this.GridGezgin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.GridGezgin.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.gezginItem,
+            this.Class});
+            this.GridGezgin.Location = new System.Drawing.Point(0, 34);
+            this.GridGezgin.Name = "GridGezgin";
+            this.GridGezgin.RowTemplate.Height = 24;
+            this.GridGezgin.Size = new System.Drawing.Size(491, 150);
+            this.GridGezgin.TabIndex = 3;
+            this.GridGezgin.Visible = false;
+            this.GridGezgin.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.GridGezgin_CellContentDoubleClick);
+            this.GridGezgin.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.GridGezgin_KeyPress);
+            // 
+            // gezginItem
+            // 
+            this.gezginItem.DataPropertyName = "ITEMCODE";
+            this.gezginItem.Frozen = true;
+            this.gezginItem.HeaderText = "Ürün";
+            this.gezginItem.Name = "gezginItem";
+            this.gezginItem.ReadOnly = true;
+            this.gezginItem.Width = 448;
+            // 
+            // Class
+            // 
+            this.Class.DataPropertyName = "CLASS";
+            this.Class.Frozen = true;
+            this.Class.HeaderText = "Class";
+            this.Class.Name = "Class";
+            this.Class.ReadOnly = true;
+            this.Class.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.GridGezgin);
+            this.Controls.Add(this.GridItems);
+            this.Controls.Add(this.BTNAktar);
+            this.Controls.Add(this.BTNAra);
+            this.Controls.Add(this.TXTCode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Cross Reference Aktarım Modülü";
+            ((System.ComponentModel.ISupportInitialize)(this.GridItems)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GridGezgin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox TXTCode;
+        private System.Windows.Forms.Button BTNAra;
+        private System.Windows.Forms.Button BTNAktar;
+        private System.Windows.Forms.DataGridView GridItems;
+        private System.Windows.Forms.DataGridView GridGezgin;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ITEM_CODE;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gezginItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Class;
     }
 }
 
--- CrossReference/Form1.cs
+++ CrossReference/Form1.cs
@@ -6,14 +6,167 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace CrossReference
 {
     public partial class Form1 : Form
     {
+        CROSSDBEntities1 db = new CROSSDBEntities1();
         public Form1()
         {
             InitializeComponent();
         }
+        #region UrunArama
+        private void gezginVeriYolla()
+        {
+            string referans = GridGezgin.CurrentRow.Cells[2].Value.ToString();
+            TXTCode.Text = GridGezgin.CurrentRow.Cells[1].Value.ToString();
+            VeriGetir(referans);
+            GridGezgin.Visible = false;
+        }
+
+        public void VeriGetir( string referans)
+        {
+          
+            if (referans==null)
+            {
+                return;
+            }
+            var model = db.CROSS.Where(x => x.CLASS.ToString() == referans).ToList();
+            GridItems.DataSource = model;
+
+        }
+        private void gezginGetir()
+        {
+            string itemCode = TXTCode.Text;
+            var modelItems = db.CROSS.Where(x => x.ITEMCODE.Replace(" ","").Contains(itemCode.Replace(" ", ""))).ToList();
+            GridGezgin.Visible = true;
+            GridGezgin.DataSource = modelItems;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            gezginGetir();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            gezginVeriYolla();
+        }
+
+        private void TXTCode_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar==13)
+            {
+                gezginGetir();
+            }
+            else if (e.KeyChar==27)
+            {
+                TXTCode.Text = "";
+            }
+            else if (e.KeyChar.Equals(Keys.Down))
+            {
+
+            }
+        }
+
+     
+
+        private void TXTCode_TextChanged(object sender, EventArgs e)
+        {
+            gezginGetir();
+        }
+
+        private void TXTCode_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Down)
+            {
+                GridGezgin.ClearSelection();
+                int index = GridGezgin.CurrentRow.Index;
+                
+                if (index!=GridGezgin.Rows.Count)
+                {
+                    index++;
+                }
+                GridGezgin.Rows[index].Selected = true;
+            }
+        }
+
+        private void TXTCode_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                GridGezgin.ClearSelection();
+                int index = GridGezgin.CurrentRow.Index;
+
+                if (index != 0)
+                {
+                    index--;
+                }
+                GridGezgin.Rows[index].Selected = true;
+            }
+        }
+
+        private void GridGezgin_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            MessageBox.Show("rafet");
+        }
+
+        private void GridGezgin_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13) GridGezgin_CellContentDoubleClick(new object(), new DataGridViewCellEventArgs(0,0));
+
+
+        }
+
+        private void GridGezgin_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            TXTCode.Text= GridGezgin.CurrentRow.Cells[1].Value.ToString();
+            GridGezgin.Visible = false;
+            VeriGetir(GridGezgin.CurrentRow.Cells[2].Value.ToString());
+        }
+        #endregion
+        #region Exel
+        private void veriAktar(string veriyolu)
+        {
+            // Create COM Objects. Create a COM object for everything that is referenced
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@veriyolu);
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+            int RowNumber = xlRange.Rows.Count;
+            int ColNumber = xlRange.Columns.Count;
+            string[,] gridVeri = new string[50, 50];
+            for (int i = 0; i < RowNumber; i++)
+            {
+                for (int j = 0; j < ColNumber; j++)
+                {
+                    gridVeri[i, j] = xlRange.Cells[i + 1, j + 1].Value2.toString();
+                }
+            }
+
+            
+
+
+        }
+        private void BTNAktar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Filter = "Excel Dosyası |*.xlsx";  
+            file.FilterIndex = 2;
+            file.RestoreDirectory = true;
+            file.CheckFileExists = false;
+            file.Title = "Excel Dosyası Seçiniz..";
+            file.ShowDialog();
+
+            string DosyaYolu = file.FileName;
+            string DosyaAdi = file.SafeFileName;
+            veriAktar(DosyaYolu);
+            
+        }
+        #endregion
     }
 }
